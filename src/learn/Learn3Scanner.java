60af3ddf95624dd50aa04896a28b9c9112c9615e
Islam <islamkwnd@gmail.com>
2023-01-22 15:24:25 +0100

    Update: Exam prep

--- src/learn/Learn3Scanner.java
+++ src/learn/Learn3Scanner.java
@@ -7,20 +7,27 @@ public class Learn3Scanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        Get data
-        System.out.print("What is your name? ");
-        String name2 = scanner.nextLine();
-//        Print data
-        System.out.printf("Hi %s. How old are you? ", name2);
-        int age = scanner.nextInt();
+////        Get data
+//        System.out.print("What is your name? ");
+//        String name2 = scanner.nextLine();
+////        Print data
+//        System.out.printf("Hi %s. How old are you? ", name2);
+//        int age = scanner.nextInt();
+//
+////        clear buffer
+//        scanner.nextLine();
+//
+//        System.out.printf("%d is an excellent age, where are you?  ", age);
+//        String location = scanner.nextLine();
+//        System.out.printf("%s is an awesome place", location);
+//
+//        scanner.close();
 
-//        clear buffer
-        scanner.nextLine();
-        
-        System.out.printf("%d is an excellent age, where are you?  ", age);
-        String location = scanner.nextLine();
-        System.out.printf("%s is an awesome place", location);
-
-        scanner.close();
+//        Another way
+//        String input = scanner.nextLine();
+//        int age = Integer.parseInt(input);
+//        double gpa = Double.parseDouble(input);
+//        float gp2 = Float.parseFloat(input);
+//        System.out.printf("%d %f %f", age, gpa, gp2);
     }
 }
